0efaa2622c1f99fe30a72ad3578d5f0a7aba0f03
dsanderson90 <dsanderson3@cnm.edu>
2020-01-15 21:51:44 -0700

    learned that you can pass a second arg to use effect that allows you specify when it should run i.e if passed [count] useEffect only is run when count state is changed.

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from 'react';
 const UseEffect = () => {
 
 const [count, setCount] = useState(0);
-
+const [name, setName] = useState('')
 // Use effect runs after the first render and after every update.
+// the effect will only run if count is altered (2nd arg)
 useEffect( () => {
+console.log('updating')
+
   document.title = `you have clicked ${count} times`
-})
+}, [count])
+
+const handleChange = (e) => {
+  const { value } = e.target
+  setName(value)
+}
 
 // This works too...
 // document.title = `you have clicked ${count} times`
@@ -14,7 +22,9 @@ useEffect( () => {
 const increment = () => setCount(prev => prev + 1)
 
   return <div>
+    {name}
     <input type='button' value={count} onClick={increment}/>
+    <input type='text' value={name} onChange={handleChange}/>
   </div>;
 };
 
