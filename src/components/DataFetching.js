fe50b09a305db54436a81e3f3a55cf9d5d2d2363
dsanderson90 <dsanderson3@cnm.edu>
2020-01-15 23:03:38 -0700

    learned how to alter a get request url and update state with useeffect

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -3,24 +3,27 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function DataFetching() {
-  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
   useEffect(() => {
     axios
-      .get('https://jsonplaceholder.typicode.com/posts')
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then(res => {
         console.log(res);
-        setPosts(res.data);
+        setPost(res.data);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [idFromButtonClick]);
 
+  const handleClick = () => {
+    setIdFromButtonClick(id)
+  }
   return (
     <div>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.body}</li>
-        ))}
-      </ul>
+      <input type='text' value={id} onChange={e => setId(e.target.value)} />
+      <input type='button' value='fetch post' onClick={handleClick} />
+      <ul>{post.title}</ul>
     </div>
   );
 }
