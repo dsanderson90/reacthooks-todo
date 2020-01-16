c8107d4057130a0566b8cb575579f474680ebcf6
dsanderson90 <dsanderson3@cnm.edu>
2020-01-15 23:37:32 -0700

    simplified code with the useContext hook

--- src/components/ComponentC.js
+++ src/components/ComponentC.js
@@ -1,24 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { UserContext, ChannelContext } from '../App';
 export default function ComponentC() {
+
+  const user = useContext(UserContext)
+  const channel = useContext(ChannelContext)
   return (
     <div>
-      CompC
-      <UserContext.Consumer>
-        {user => {
-          return (
-            <ChannelContext.Consumer>
-              {channel => {
-                return (
-                  <div>
-                    Channel Context Value {channel}, User context value {user}
-                  </div>
-                );
-              }}
-            </ChannelContext.Consumer>
-          );
-        }}
-      </UserContext.Consumer>
+      {user}
+      {channel}
     </div>
   );
 }
